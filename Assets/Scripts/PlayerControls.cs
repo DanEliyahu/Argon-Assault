f18690f82fa698b3532211ce594a5caf846276da
Dan Eliahu <daneliyahu@cs.huji.ac.il>
2021-06-16 17:53:58 +0300

    Player Movement

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -2,15 +2,22 @@
 
 public class PlayerControls : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float controlSpeed = 10f;
+    [SerializeField] private float xRange = 10f;
+    [SerializeField] private float yTopLimit = 11f;
+    [SerializeField] private float yBottomLimit = -3f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        var localPosition = transform.localPosition;
+        var xThrow = Input.GetAxis("Horizontal");
+        var yThrow = Input.GetAxis("Vertical");
+        var xOffset = xThrow * controlSpeed * Time.deltaTime;
+        var yOffset = yThrow * controlSpeed * Time.deltaTime;
+        var newXPos = Mathf.Clamp(localPosition.x + xOffset, -xRange, xRange);
+        var newYPos = Mathf.Clamp(localPosition.y + yOffset, yBottomLimit, yTopLimit);
+
+        transform.localPosition = new Vector3(newXPos, newYPos);
     }
 }
