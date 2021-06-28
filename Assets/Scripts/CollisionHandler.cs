19993c9d7f7275c2b9f3b93150152c775edaabd8
Dan Eliahu <daneliyahu@cs.huji.ac.il>
2021-06-28 19:08:34 +0300

    Collisions and Explosion VFX

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    [SerializeField] private float levelLoadDelay = 1f;
+
+    private bool _isTransitioning = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_isTransitioning)
+        {
+            return;
+        }
+
+        StartCrashSequence();
+    }
+
+    private void StartCrashSequence()
+    {
+        _isTransitioning = true;
+        print("Crash");
+        GetComponent<PlayerControls>().enabled = false;
+        Invoke(nameof(ReloadLevel), levelLoadDelay);
+    }
+
+    private void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
